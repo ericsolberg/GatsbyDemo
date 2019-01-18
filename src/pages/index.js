46857311896892f9ec92bb2ff0ba39752c5dcad6
Eric Solberg <eric.solberg@sap.com>
2019-01-18 09:10:44 -0800

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+
+const HomePage = () => (
+  <StaticQuery
+    query={graphql`
+      query HomePage {
+        contentfulHomePage {
+          title
+          date
+          content {
+            childMarkdownRemark {
+              html
+            }
+          }
+          image {
+            file {
+              url
+            }
+          }
+        }
+      }
+    `}
+    render={({
+      contentfulHomePage: {
+        title,
+        date,
+        content: {
+          childMarkdownRemark: { html }
+        },
+        image: {
+          file: { url }
+        }
+      }
+    }) => (
+      <>
+        <h1>{title}</h1>
+        <small>Created on {date}</small><br />
+        <img src={url} />
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </>
+    )}
+  />
+);
+
+export default HomePage;
